348bfa15f1cc26690122291e082a51f765873c1a
DryCryCrystal <ryan2401@hotmail.co.uk>
2015-01-05 15:35:44 +0000

    Tried to fix elemental arrow proj
    Need help!

--- Projectiles/ElementalArrowProj.cs
+++ Projectiles/ElementalArrowProj.cs
@@ -10,19 +10,10 @@ namespace Elemental.Projectiles
 {
     public class ElementalArrowProj : ModProjectile
     {
-        public override void AI()
-        {
-			projectile.rotation = (float)System.Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 1.57f;
-        }
-
-        public override void PostKill() //spawns dust on collision and plays a sound.
-        {
-            Collision.HitTiles(projectile.position, projectile.velocity, projectile.width, projectile.height); //tile dust
-            Main.PlaySound(0, (int)projectile.position.X, (int)projectile.position.Y, 1);
-            for (int m = 0; m < 5; m++) //projectile dust
-            {
-                int dustID = Dust.NewDust(projectile.position, projectile.width, projectile.height, 30, projectile.velocity.X * 0.2f, projectile.velocity.Y * 0.2f, 100, Color.Violet, 1.2f);
-            }
-        }
-    }
+	public override void AI()
+		{
+			int dust = Dust.NewDust(new Vector2((float) position.X, (float) position.Y), Width, Height, 6, 0, 0, 100, Color.White, 1f);
+			Main.dust[dust].noGravity = true;
+		}
+	}
 }
