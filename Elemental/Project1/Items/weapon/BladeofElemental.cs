8f251435a36f69d15c3cd2912cdd7e106cdc2c1b
DryCryCrystal <ryan2401@hotmail.co.uk>
2015-02-01 19:23:57 +0000

    completely changed everything for visual studio

---
+++ Elemental/Project1/Items/weapon/BladeofElemental.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.Xna.Framework;
+
+using TAPI;
+using Terraria;
+
+namespace Elemental.Items
+{
+public class BladeofElemental : ModItem
+	{
+	public override void UseItemEffects(Player player, Rectangle rectangle)
+		{
+            int dust = Dust.NewDust(new Vector2((float) rectangle.X, (float) rectangle.Y), rectangle.Width, rectangle.Height, 75, player.velocity.X, player.velocity.Y, 100, Color.Gold, 1f);
+            Main.dust[dust].noGravity = true;
+		}
+	}
+}
---
+++ Elemental/Project1/Projectiles/ElementalArrowProj.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+using TAPI;
+using Terraria;
+
+namespace Elemental.Projectiles
+{
+    public class ElementalArrowProj : ModProjectile
+    {
+	public override void AI()
+		{
+			int dust = Dust.NewDust(new Vector2((float) projectile.position.X, (float) projectile.position.Y), projectile.width, projectile.height, 75, 0, 0, 100, Color.White, 1f);
+			Main.dust[dust].noGravity = true;
+		}
+	}
+}
